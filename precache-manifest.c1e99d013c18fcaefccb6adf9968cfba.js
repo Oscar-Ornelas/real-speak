045b8ac6725352b3be01de714d86176574117dde
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-03-14 17:45:43 -0600

    Updates

---
+++ precache-manifest.c1e99d013c18fcaefccb6adf9968cfba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b469f88b2a4adb5bc75cb251d08859",
+    "url": "/real-speak/index.html"
+  },
+  {
+    "revision": "d0b14df74b75230123eb",
+    "url": "/real-speak/static/css/main.d619f9c0.chunk.css"
+  },
+  {
+    "revision": "69adfaed79ce99c46312",
+    "url": "/real-speak/static/js/2.ccbea44c.chunk.js"
+  },
+  {
+    "revision": "c310a938bc930240e265a9711b814ed3",
+    "url": "/real-speak/static/js/2.ccbea44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b14df74b75230123eb",
+    "url": "/real-speak/static/js/main.9602dc9c.chunk.js"
+  },
+  {
+    "revision": "f23be3929f1fe99d4ba4",
+    "url": "/real-speak/static/js/runtime-main.1d76ea08.js"
+  }
+]);
