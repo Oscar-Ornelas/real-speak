53e38a1a41146b894723334085a03b479d7408db
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-03-22 19:52:31 -0600

    Add landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useAuth0 } from "../react-auth0-spa";
 
 function Landing() {
-  const {loginWithRedirect} = useAuth0;
+  const {loginWithRedirect} = useAuth0();
 
   return (
     <div className="landing">
-      <h1>RealSpeak</h1>
-      <h2>A new way to chat with your communities and friends.</h2>
-      <p>RealSpeak is the easiest way to communicate over voice, video, and text,
+      <h1 className="logo">RealSpeak</h1>
+      <h2 className="landing-header">A new way to chat with your communities and friends.</h2>
+      <p className="landing-subtitle">RealSpeak is the easiest way to communicate over voice, video, and text,
       whether you’re part of a school club, a nightly gaming group, a worldwide
       art community, or just a handful of friends that want to hang out.
       </p>
