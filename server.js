b3d5ef77eb9ca8976c72f7d08ac7cd8ae05de35b
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-06-16 14:02:14 -0500

    Add heroku url as allowed cors origin

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const index = require("./routes/index");
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
 
+var allowedOrigins = ['http://localhost:3000',
+                      'https://real-speak.herokuapp.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(index);
 app.use(express.json());
 
