4dd52797a31fa95925e831e49865b2876f10338a
Oscar Ornelas <oscarornelas98123@gmail.com>
2021-10-06 16:52:48 -0500

    Update client credentials

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ const options = {
   headers: {'content-type': 'application/x-www-form-urlencoded'},
   form: {
     grant_type: 'client_credentials',
-    client_id: '0Ha63pjQGY1ao3Hveh8kIOM0biYukl60',
-    client_secret: process.env.CLIENT_SECRET,
+    client_id: '8Yjxilx9qldnHipHryE81gsbj09qLJb6',
+    client_secret: '-KEN7h9qjLhW2tYEMnbzavnOE3lAwzANfxxVeAkorwzh2WQOnE2wi3xqtGCG1a5q',
     audience: 'https://dev-pdp1v9a4.auth0.com/api/v2/'
   }
 };
